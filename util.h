cff8df4d7c8c20f0c5f52bcd378438661be48b57
= <=>
2019-04-17 22:59:17 +0500

    Util.h started

---
+++ util.h
@@ -0,0 +1,16 @@
+#ifndef _UTIL_HEAD
+#define _UTIL_HEAD
+
+namespace Utility{
+    template <class type>
+    bool CheckArray(type*array, unsigned size){
+        for (unsigned i=0;i<size;++i)
+            if (!array[i])
+                return 0;
+        return 1;
+    }
+
+    
+}
+
+#endif
