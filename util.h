f01138f67e0cb272e3de70be5330bf9e7b3711a7
= <=>
2019-04-17 23:05:50 +0500

    Added print function

--- util.h
+++ util.h
@@ -1,3 +1,5 @@
+#include <iostream>
+
 #ifndef _UTIL_HEAD
 #define _UTIL_HEAD
 
@@ -8,7 +10,17 @@ namespace Utility{
             if (!array[i])
                 return 0;
         return 1;
-    }    
+    }
+
+    template <class type>
+    void Print2DArray(type ** array,unsigned dim_1,unsigned dim_2){
+        for (unsigned i=0;i<dim_1;++i){
+            for (unsigned j=0;j<dim_2;++j){
+                std::cout << array[i][j] << " ";
+            }
+            std::cout << std::endl;
+        }
+    }  
 }
 
 #endif
