c36be6fbe2ec6ddd82581522bbef3aef03042fc3
= <=>
2019-04-17 23:12:06 +0500

    Thread function containers added

---
+++ thread.hpp
@@ -0,0 +1,24 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <wait.h>
+#include <stdio.h>
+#include <semaphore.h>
+
+#ifndef _THREAD_FUNCTIONS_
+#define _THREAD_FUNCTIONS_
+
+namespace ThreadFunctions{
+    void * ColumnChecker(void * args){
+
+    }
+
+    void * RowChecker(void * args){
+
+    }
+
+    void * RegionChecker(void * args){
+        
+    }
+}
+
+#endif
