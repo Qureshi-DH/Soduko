f0009b2ab10846aa84332ab3307ca2cd82ab6aa7
= <=>
2019-04-17 23:25:27 +0500

    Column checker implemeneted

--- thread.hpp
+++ thread.hpp
@@ -4,12 +4,24 @@
 #include <stdio.h>
 #include <semaphore.h>
 
+#include "grid.hpp"
+#include "util.h"
+#include "tuple.h"
+
+using namespace Utility;
+
 #ifndef _THREAD_FUNCTIONS_
 #define _THREAD_FUNCTIONS_
 
 namespace ThreadFunctions{
     void * ColumnChecker(void * args){
-
+        unsigned short test_array[9] = {0};
+        unsigned column = *((unsigned*)args);
+        for (unsigned i=0;i<9;++i){
+            test_array[Soduko::grid[i][column]]++;
+        }
+        bool status = Utility::CheckArray(test_array,9);
+        pthread_exit(&status);
     }
 
     void * RowChecker(void * args){
@@ -17,7 +29,7 @@ namespace ThreadFunctions{
     }
 
     void * RegionChecker(void * args){
-        
+
     }
 }
 
