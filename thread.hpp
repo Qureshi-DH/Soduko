7735624c7cdfb7562b6d69f8aa197d99e34d399f
= <=>
2019-04-18 15:15:47 +0500

    Major bugs removed

--- thread.hpp
+++ thread.hpp
@@ -17,10 +17,14 @@
 
 namespace ThreadFunctions{
     void * ColumnChecker(void * args){
+        unsigned test_array[9] = {0};
         unsigned & column = *((unsigned*)args);
         static Tuple invalid_position = Tuple(-1,-1);
         for (unsigned i=0;i<9;++i){
             if (Soduko::grid[i][column] > 9 || Soduko::grid[i][column] < 1){
+                invalid_position = Tuple(i,column);
+            }
+            else if (++test_array[Soduko::grid[i][column]-1] != 1){
                 invalid_position = Tuple(i,column);
             }
         }
@@ -28,10 +32,14 @@ namespace ThreadFunctions{
     }
 
     void * RowChecker(void * args){
+        unsigned test_array[9] = {0};
         unsigned & row = *((unsigned*)args);
         static Tuple invalid_position = Tuple(-1,-1);
         for (unsigned i=0;i<9;++i){
             if (Soduko::grid[row][i] > 9 || Soduko::grid[row][i] < 1){
+                invalid_position = Tuple(row,i);
+            }
+            else if (++test_array[Soduko::grid[row][i]-1] != 1){
                 invalid_position = Tuple(row,i);
             }
         }
@@ -39,11 +47,15 @@ namespace ThreadFunctions{
     }
 
     void * RegionChecker(void * args){
+        unsigned test_array[9] = {0};
         Tuple & tuple  = *((Tuple*)args);
         static Tuple invalid_position = Tuple(-1,-1);
         for (unsigned i=tuple.row*3,k=0;k<3;++i,++k){
             for (unsigned j=tuple.col*3,l=0;l<3;++j,++l){
                 if (Soduko::grid[i][j] < 1 || Soduko::grid[i][j] > 9){
+                    invalid_position = Tuple(i,j);
+                }
+                else if (++test_array[Soduko::grid[i][j]-1] != 1){
                     invalid_position = Tuple(i,j);
                 }
             }
@@ -60,8 +72,8 @@ std::vector <Tuple> invalid_boxes;
 
 void CreateThreads(){
     using namespace ThreadStructs;
-    int array[9] = {1,2,3,4,5,6,7,8,9};
-    Tuple region[9];
+    static unsigned array[9] = {0,1,2,3,4,5,6,7,8};
+    static Tuple region[9];
     for (short i=0,j=-1;i<9;++i){
         j = i%3==0?++j:j;
         region[i] = Tuple(j,i%3);
@@ -75,8 +87,8 @@ bool JoinThreads(){
     void * status;
     Validity current_status = Validity::Valid;
     for (short i=0;i<9;++i){
-        Tuple & received_position = *((Tuple*)status);
         pthread_join(ThreadStructs::row_threads[i],&status);
+        Tuple & received_position = *((Tuple*)status);
         if (received_position.row != -1 && received_position.col != -1){
             invalid_boxes.push_back(received_position);
             current_status = Validity::Invalid;
