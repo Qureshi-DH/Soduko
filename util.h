2c5c9004eaa816f5f7f7c08adc484294794cda2e
= <=>
2019-04-18 15:42:59 +0500

    Implemented CheckIfExists() method

--- util.h
+++ util.h
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 #ifndef _UTIL_HEAD
 #define _UTIL_HEAD
@@ -13,14 +14,22 @@ namespace Utility{
     }
 
     template <class type>
-    void Print2DArray(type ** array,unsigned dim_1,unsigned dim_2){
+    void Print2DArray(type array[9][9],unsigned dim_1,unsigned dim_2){
         for (unsigned i=0;i<dim_1;++i){
             for (unsigned j=0;j<dim_2;++j){
                 std::cout << array[i][j] << " ";
             }
             std::cout << std::endl;
         }
-    }  
+    }
+
+    template <class type>
+    bool CheckIfExists(std::vector<type> & list,type element){
+        for (size_t i=0;i<list.size();++i)
+            if (list[i] == element)
+                return 1;
+        return 0;
+    }
 }
 
 enum Validity{
