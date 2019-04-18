f30871619d9db6b5db46ab6f758318f516956683
= <=>
2019-04-18 15:41:01 +0500

    Overloaded == operator

--- tuple.h
+++ tuple.h
@@ -8,4 +8,8 @@ struct Tuple{
     Tuple(int _row,int _col):row(_row),col(_col){}
 };
 
+bool operator==(const Tuple& left,const Tuple& right){
+    return ((left.row == right.row) && (left.col == right.col));
+}
+
 #endif
