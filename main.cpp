f6bf0f052ffc53b9c25337e3aaa24152e1232c26
= <=>
2019-04-21 01:50:28 +0500

    Main() finalised

--- main.cpp
+++ main.cpp
@@ -5,10 +5,15 @@
 
 #include "grid.hpp"
 #include "util.h"
+#include "thread.hpp"
 
-//using namespace std;
+using namespace GLOBAL_DATA;
 
 int main(int argc,char*argv[]){
-
-    return 0;
+    sem_init(&GLOBAL_DATA::novux,0,1);
+    CreateThreads();
+    bool status = JoinThreads();
+    Utility::Print2DArray(Soduko::grid,9,GLOBAL_DATA::invalid_boxes);
+    std::cout << "Validity = " << (status?("\033[1;32mValid\033[0m"):("\033[1;31mInvalid\033[0m")) << std::endl;
+    pthread_exit(NULL);
 }
